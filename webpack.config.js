188f866c1cb5845b4ce57e24769092bb7f73022f
keisuke-onpa <k-sasaki@onpa-japan.com>
2019-01-25 18:54:41 +0900

    static/jsにある不要なファイルは全部消す

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const version = (+new Date)
 const fs = require("fs");
+const new_filename = 'app.' + version + '.js';
 
 module.exports = {
 	watch: true,
@@ -7,7 +8,7 @@ module.exports = {
 	entry: "./assets/app.js",
 	output: {
 		path: `${__dirname}/static/js`,
-		filename: "app." + version + ".js"
+		filename: new_filename
 	},
 	module: {
 		rules: [
@@ -36,6 +37,15 @@ module.exports = {
 }
 
 let static_file = JSON.parse(fs.readFileSync(`${__dirname}/static_file_version.json`));
+let filenames = fs.readdirSync(`${__dirname}/static/js/`);
+for(filenum = 0; filenum < filenames.length; filenum++) {
+	if(filenames[filenum] != new_filename) {
+		fs.unlink(`${__dirname}/static/js/${filenames[filenum]}`, function(){});
+	}
+}
+static_file.version = version;
+fs.writeFileSync(`${__dirname}/static_file_version.json`, JSON.stringify(static_file, null, ''));
+
 let old_version = static_file.version;
 static_file.version = version;
 fs.writeFileSync(`${__dirname}/static_file_version.json`, JSON.stringify(static_file, null, ''));
